c44e47ec508d0848b797569a516660fc48116153
Sangharsh <sangharsh.wagle@libertymutual.com>
2022-05-16 09:27:50 -0500

    Initial commit

---
+++ src/main/java/com/sangharsh/GithubTopics/GithubTopicsApplication.java
@@ -0,0 +1,13 @@
+package com.sangharsh.GithubTopics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubTopicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubTopicsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sangharsh/GithubTopics/GithubTopicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sangharsh.GithubTopics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubTopicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
